c1c337ed51382bfe94149425b3ed8b9339ddc190
Kenanga1-sch <uptdsdn1kenangasindang@gmail.com>
2026-06-02 20:31:33 +0700

    fix: refresh service worker cache after deploy

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /// <reference lib="webworker" />
 
-const CACHE_NAME = 'sekolahku-v3';
+const CACHE_NAME = 'sekolahku-v4';
 const OFFLINE_URL = '/offline';
 
 // Assets to cache immediately on install
